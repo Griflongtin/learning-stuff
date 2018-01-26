c8210c0e1bfc708ed20d72a235d49d88dbb7af41
Griffin P Longtin <griflongtin@live.com>
2018-01-26 11:27:47 -0800

    add imports the the app.routing file

---
+++ src/app/app.routing.ts
@@ -0,0 +1,5 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { WelcomeComponent } from './welcome/welcome.component';
+import { WorksComponent } from './works/works.component';

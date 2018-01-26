90506481dbf5769b2d2073501b826068b2a3d0f7
Griffin P Longtin <griflongtin@live.com>
2018-01-26 13:27:31 -0800

    add the injectable to the welcome component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserDocsService } from '../user-docs.service';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
+  providers: [UserDocsService]
 })
 export class WelcomeComponent implements OnInit {
 

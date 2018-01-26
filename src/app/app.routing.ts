ae9445e0c87fa73b80bb143acae68ef414b9b382
Griffin P Longtin <griflongtin@live.com>
2018-01-26 11:29:44 -0800

    add the export to the app.routing file

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,3 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 import { WorksComponent } from './works/works.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+
+  },
+  {
+     path: 'work',
+     component: WorksComponent
+   }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
